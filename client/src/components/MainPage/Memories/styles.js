b8f87b1081059f8e5ce04d45c87a0a2d17d70a3f
moh-sa <tno.rappelz@gmail.com>
2023-01-02 17:00:50 +0300

    centered the cards

--- client/src/components/MainPage/Memories/styles.js
+++ client/src/components/MainPage/Memories/styles.js
@@ -3,6 +3,7 @@ import { createStyles } from "@mantine/core";
 export const useStyles = createStyles((theme) => ({
   GridCol: {
     display: "flex",
+    justifyContent: "center",
     flexGrow: 1,
   },
 }));
