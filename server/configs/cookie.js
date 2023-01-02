cf00d66b3f73efefb6805410c3ef7d7960b17a47
moh-sa <tno.rappelz@gmail.com>
2023-01-02 17:02:03 +0300

    changed "sameSite" to "lax" to fix a cookie's bug

--- server/configs/cookie.js
+++ server/configs/cookie.js
@@ -1,17 +1,15 @@
 const access = {
   name: process.env.COOKIE_ACCESS_NAME,
   options: {
-    sameSite: "none",
-    domain:
-      process.env.NODE_ENV === "development" ? "localhost" : ".tno-msa.com",
+    sameSite: "lax",
+    domain: process.env.NODE_ENV === "development" ? "localhost" : ".tno.dev",
     httpOnly: false,
     secure: process.env.NODE_ENV === "development" ? false : true,
     maxAge: process.env.ACCESS_EXP,
   },
   delete: {
-    sameSite: "none",
-    domain:
-      process.env.NODE_ENV === "development" ? "localhost" : ".tno-msa.com",
+    sameSite: "lax",
+    domain: process.env.NODE_ENV === "development" ? "localhost" : ".tno.dev",
     httpOnly: false,
     secure: process.env.NODE_ENV === "development" ? false : true,
     maxAge: new Date(null),
@@ -21,17 +19,15 @@ const access = {
 const refresh = {
   name: process.env.COOKIE_REFRESH_NAME,
   options: {
-    sameSite: "none",
-    domain:
-      process.env.NODE_ENV === "development" ? "localhost" : ".tno-msa.com",
+    sameSite: "lax",
+    domain: process.env.NODE_ENV === "development" ? "localhost" : ".tno.dev",
     httpOnly: true,
     secure: process.env.NODE_ENV === "development" ? false : true,
     maxAge: process.env.REFRESH_EXP,
   },
   delete: {
-    sameSite: "none",
-    domain:
-      process.env.NODE_ENV === "development" ? "localhost" : ".tno-msa.com",
+    sameSite: "lax",
+    domain: process.env.NODE_ENV === "development" ? "localhost" : ".tno.dev",
     httpOnly: true,
     secure: process.env.NODE_ENV === "development" ? false : true,
     maxAge: new Date(null),
