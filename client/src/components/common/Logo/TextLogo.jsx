a4d0c9a021cf2472e5d475e18c1715e64d67f5ed
moh-sa <tno.rappelz@gmail.com>
2023-01-11 14:12:43 +0300

    fixed the styles

--- client/src/components/common/Logo/TextLogo.jsx
+++ client/src/components/common/Logo/TextLogo.jsx
@@ -3,8 +3,12 @@ import { Text, Box } from "@mantine/core";
 
 const TextLogo = () => {
   return (
-    <Box component={Link} to="/">
-      <Text weight={500} align="center">
+    <Box
+      component={Link}
+      to="/"
+      style={{ textDecoration: "none", color: "inherit" }}
+    >
+      <Text weight={500} size="1.47em">
         Memories
       </Text>
     </Box>
