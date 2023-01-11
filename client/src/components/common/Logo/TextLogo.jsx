0f3c026b0e8a0d0068c0d72bf95a2472b70eafb8
moh-sa <tno.rappelz@gmail.com>
2023-01-11 13:02:21 +0300

    Replaced the img with text

--- client/src/components/common/Logo/TextLogo.jsx
+++ client/src/components/common/Logo/TextLogo.jsx
@@ -1,20 +1,12 @@
 import { Link } from "react-router-dom";
-import { Image, Text, Box, useMantineColorScheme } from "@mantine/core";
-import WhiteLogo from "./assets/WHITE-LOGO.svg";
-import BlackLogo from "./assets/BLACK-LOGO.svg";
+import { Text, Box } from "@mantine/core";
 
 const TextLogo = () => {
-  const { colorScheme } = useMantineColorScheme();
-
   return (
     <Box component={Link} to="/">
-      <Image
-        src={colorScheme === "dark" ? WhiteLogo : BlackLogo}
-        alt="Memories text logo"
-        width="8em"
-        withPlaceholder
-        placeholder={<Text align="center">Memories text logo</Text>}
-      />
+      <Text weight={500} align="center">
+        Memories
+      </Text>
     </Box>
   );
 };
