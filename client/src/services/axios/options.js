e2155e6d788b416ac35e10d54833d4d5e63776b3
MSA <46880411+moh-sa@users.noreply.github.com>
2023-04-10 01:50:55 +0300

    dynamic back-end url

--- client/src/services/axios/options.js
+++ client/src/services/axios/options.js
@@ -3,14 +3,8 @@ import axios from "axios";
 const URL =
   process.env.NODE_ENV === "development"
     ? process.env.REACT_APP_BACK_DEV_URL
-    : `https://mempapi.${window.location.hostname.split("memories.")[1]}`;
+    : `https://memoapi.${window.location.hostname.split("memories.")[1]}`;
 //  : process.env.REACT_APP_BACK_URL
-
-console.log(window.location.hostname.split("memories."));
-console.log(window.location.hostname.split("memories.")[1]);
-console.log(
-  `https://mempapi.${window.location.hostname.split("memories.")[1]}`
-);
 
 export const API = axios.create({
   baseURL: URL,
