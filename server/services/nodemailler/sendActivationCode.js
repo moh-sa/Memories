a19888953faeb55ae5cdeaf51adf80d41dbe8149
MSA <46880411+moh-sa@users.noreply.github.com>
2023-04-10 02:27:27 +0300

    changed the message and URL

--- server/services/nodemailler/sendActivationCode.js
+++ server/services/nodemailler/sendActivationCode.js
@@ -3,19 +3,19 @@ import client from "./options.js";
 const domain =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : "https://memories.tno.dev";
+    : "https://memories.moh-sa.dev";
 
 const sendEmail = async (name, email, code) => {
   const link = `${domain}/activation?code=${code}`;
   const sentEmail = await client.sendMail({
-    from: `"Memories Family" <${process.env.EMAIL}>`,
+    from: `"Memories" <${process.env.EMAIL}>`,
     to: email,
-    subject: "Memories - Account Activation",
+    subject: "Memories - Almost There! Confirm Your Registration ",
     text: "Hello world?",
-    html: `<div><h4>Hello ${name}👋,</h4></div>
+    html: `<div><h4>Hello and welcome, ${name}! 👋</h4></div>
         <div>
-        <p>We are glad to have you in the Memories family!</p>
-        <p>In order to confirm your registration, we kindly ask you to activate your account by clicking on the link below:</p>
+        <p>We're thrilled to have you join the Memories family!</p>
+        <p>To get started on your journey with us, please take a moment to confirm your registration by clicking the link below:</p>
         </div>
         <div><a href=${link} alt="activation link">${link}</a></div>`,
   });
