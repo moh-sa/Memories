9292981141b93545b2985951770a19e4f673b786
MSA <46880411+moh-sa@users.noreply.github.com>
2023-04-10 02:05:27 +0300

    array of URLs

--- server/configs/cors.js
+++ server/configs/cors.js
@@ -1,6 +1,11 @@
-import URL from "./url.js";
+// import URL from "./url.js";
+//origin: URL.FRONT
 
 export default {
   credentials: true,
-  origin: URL.FRONT,
+  origin: [
+    "http://localhost:3000",
+    "https://memories.moh-sa.dev",
+    "https://memories.tno.dev",
+  ],
 };
