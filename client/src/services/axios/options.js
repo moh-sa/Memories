f7995873d36f15c7ef5976591180506d2ace7fbe
MSA <46880411+moh-sa@users.noreply.github.com>
2023-04-10 01:48:00 +0300

    Update options.js

--- client/src/services/axios/options.js
+++ client/src/services/axios/options.js
@@ -6,6 +6,12 @@ const URL =
     : `https://mempapi.${window.location.hostname.split("memories.")[1]}`;
 //  : process.env.REACT_APP_BACK_URL
 
+console.log(window.location.hostname.split("memories."));
+console.log(window.location.hostname.split("memories.")[1]);
+console.log(
+  `https://mempapi.${window.location.hostname.split("memories.")[1]}`
+);
+
 export const API = axios.create({
   baseURL: URL,
   withCredentials: true,
