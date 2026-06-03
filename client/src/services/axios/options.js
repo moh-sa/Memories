2741f2fc41d227a6b7dd2d86cac4f66dc87f4514
Tno <me@moh-sa.dev>
2026-06-03 20:47:32 +0300

    fix(client): set production API URL from env

--- client/src/services/axios/options.js
+++ client/src/services/axios/options.js
@@ -3,8 +3,7 @@ import axios from "axios";
 const URL =
   process.env.NODE_ENV === "development"
     ? process.env.REACT_APP_BACK_DEV_URL
-    : `https://memoapi.${window.location.hostname.split("memories.")[1]}`;
-//  : process.env.REACT_APP_BACK_URL
+    : process.env.REACT_APP_BACK_URL;
 
 export const API = axios.create({
   baseURL: URL,
